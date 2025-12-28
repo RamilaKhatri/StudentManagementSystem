e271cd199c244e47e7873d22efa54d7ae439c1c6
RamilaKhatri <ramilak373@gmail.com>
2025-12-28 21:15:17 +0545

    Added getter methods to Student class

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    String name;
+    int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+}
